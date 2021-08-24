34a591a77d49720a41199978c92e607b583f7400
proistefan <i.stefan.pm@gmail.com>
2021-08-24 15:45:12 -0400

    fix: layout

--- components/PostBody.js
+++ components/PostBody.js
@@ -1,6 +1,6 @@
 export default function PostBody({ content }) {
   return (
-    <div className="prose">
+    <div className="max-w-4xl prose">
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   )
