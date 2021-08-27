e276daaddb413e429659f700ee430035fb9b91d7
proistefan <i.stefan.pm@gmail.com>
2021-08-27 18:00:22 -0400

    feat: captcha working + toast

--- components/SignupCard.js
+++ components/SignupCard.js
@@ -3,7 +3,6 @@ import { useForm } from 'react-hook-form'
 
 import Success from './Success'
 import toast, { Toaster } from 'react-hot-toast'
-const notify = () => toast(<Success />, {})
 
 import HCaptcha from '@hcaptcha/react-hcaptcha'
 
@@ -25,24 +24,22 @@ export default function SignupCard() {
 
   useEffect(() => {
     if (token) {
-      // Token is set, can submit here
-      console.log(`User Email: ${email}`)
-      console.log(`hCaptcha Token: ${token}`)
+      const data = { email: `${email}` }
+
+      fetch('/api/form', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+
+      toast(<Success />, {})
     }
   }, [token, email])
 
-  // submit form
-
-  const onSubmit = async (data) => {
+  const onSubmit = () => {
     captchaRef.current.execute()
-
-    const res = await fetch('/api/form', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
   }
 
   return (
@@ -57,36 +54,43 @@ export default function SignupCard() {
       </div>
       <div className="mt-2">
         <form className="sm:flex" onSubmit={handleSubmit(onSubmit)}>
-          <label for="email" className="sr-only">
-            your@email.com
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            autocomplete="email"
-            className="w-full px-5 py-3 placeholder-gray-500 border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-brand-700 focus:ring-white"
-            placeholder="Enter your email"
-            aria-describedby="email-description"
-            ref={register({ required: true, maxLength: 80 })}
-            onChange={(evt) => setEmail(evt.target.value)}
-          />
-          <button
-            onClick={notify}
-            type="submit"
-            className="flex items-center justify-center w-full px-5 py-3 mt-3 text-base font-medium text-white border border-transparent rounded-md shadow bg-brand-500 hover:bg-brand-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-brand-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0"
-          >
-            Join now
-          </button>
-          <HCaptcha
-            // This is testing sitekey, will autopass
-            // Make sure to replace
-            sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY}
-            onVerify={setToken}
-            onError={onError}
-            onExpire={onExpire}
-            ref={captchaRef}
-          />
+          <div className="flex flex-col">
+            <div className="inline-flex">
+              <label for="email" className="sr-only">
+                your@email.com
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autocomplete="email"
+                className="w-full px-5 py-3 placeholder-gray-500 border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-brand-700 focus:ring-white"
+                placeholder="Enter your email"
+                aria-describedby="email-description"
+                ref={register({ required: true, maxLength: 80 })}
+                value={email}
+                onChange={(evt) => setEmail(evt.target.value)}
+              />
+              <button
+                type="submit"
+                className="flex items-center justify-center w-full px-5 py-3 mt-3 text-base font-medium text-white border border-transparent rounded-md shadow bg-brand-500 hover:bg-brand-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-brand-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0"
+              >
+                Join now
+              </button>
+            </div>
+            <div className="mt-2">
+              <HCaptcha
+                // This is testing sitekey, will autopass
+                // Make sure to replace
+                sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY}
+                onVerify={setToken}
+                onError={onError}
+                onExpire={onExpire}
+                ref={captchaRef}
+                size="invisible"
+              />
+            </div>
+          </div>
           <Toaster
             toastOptions={{
               className: 'rounded-md bg-green-50 p-4',
